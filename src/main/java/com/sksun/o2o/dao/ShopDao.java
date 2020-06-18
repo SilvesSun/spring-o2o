7f96ec2f081e4c7c9b4ff46ea001ae09c6cb4733
silvesSun <2498256234@qq.com>
2020-06-18 23:45:06 +0800

    shop dao test

--- src/main/java/com/sksun/o2o/dao/ShopDao.java
+++ src/main/java/com/sksun/o2o/dao/ShopDao.java
@@ -3,6 +3,10 @@ package com.sksun.o2o.dao;
 import com.sksun.o2o.entity.Shop;
 
 public interface ShopDao {
-
+    /**
+     *add shop
+     * @param shop
+     * @return
+     */
     int insertShop(Shop shop);
 }
